67a39c4369ee67cef2a7ce84f0a1db73b4891d71
knweston <kevin.weston@tamu.edu>
2021-04-12 13:33:16 -0500

    added micro_trainer to tf compilation tree

---
+++ tensorflow/lite/micro/training/micro_training.cc
@@ -0,0 +1,10 @@
+#include "tensorflow/lite/micro/training/micro_training.h"
+#include <iostream>
+
+namespace tflite {
+
+void train() {
+    std::cout << "hello from micro_training" << std::endl;
+}
+
+}
---
+++ tensorflow/lite/micro/training/micro_training.h
@@ -0,0 +1,9 @@
+#include "tensorflow/lite/micro/micro_interpreter.h"
+
+#ifndef TENSORFLOW_LITE_MICRO_MICRO_TRAINER_H_
+#define TENSORFLOW_LITE_MICRO_MICRO_TRAINER_H_
+
+namespace tflite {
+void train();
+}
+#endif // TENSORFLOW_LITE_MICRO_MICRO_TRAINER_H_
